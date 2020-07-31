c89a6ad42a8e10fda140f682e77017ad62ecab4c
Logan McMillan <logan.t.mcmillan@gmail.com>
2020-07-30 22:07:30 -0400

    Added makeshift chopping.

---
+++ Playground/Assets/WoodChop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodChop : MonoBehaviour
+{
+	private bool inUse;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (inUse && other.gameObject.CompareTag("Generated"))
+		{
+			Destroy(other.gameObject);
+		}
+	}
+
+	public void Use()
+	{
+		inUse = true;
+	}
+
+	public void StopUse()
+	{
+		inUse = false;
+	}
+}
